df3b4c4409d10b431e07946ed7fd6198eb792f2a
Dzmitry Horbach <dim.horbach@gmail.com>
2014-12-25 15:25:05 +0300

    kafka config vars based on env

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-sed -i "s/zookeeper.connect.*/zookeeper.connect=$ZOOKEEPER_CONNECT/g" config/server.properties
+# taken from https://github.com/wurstmeister/kafka-docker/blob/master/start-kafka.sh
+for VAR in `env`
+do
+  if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME && ! $VAR =~ ^KAFKA_VERSION ]]; then
+    kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
+    env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
+    if egrep -q "(^|^#)$kafka_name" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+    else
+        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+    fi
+  fi
+done
 
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
