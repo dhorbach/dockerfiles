c40b9ce072b869e4b0d5e74fc72e602f3c1028e4
Dzmitry Horbach <dim.horbach@gmail.com>
2014-12-30 10:46:06 +0300

    updated solr container with 4.10.3

--- solr/solr-wrapper.sh
+++ solr/solr-wrapper.sh
@@ -4,4 +4,4 @@ if [ ! -f /opt/solr/example/solr/solr.xml ]; then
     cp /opt/solr/solr.xml /opt/solr/example/solr/solr.xml
 fi
 
-/opt/solr/bin/solr "$@"
+/opt/solr/bin/solr -f -a -Djetty.maxidletime=600000
