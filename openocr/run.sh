0fc98c03835ca40cce89fc18dbeeb5db7c278742
Dzmitry Horbach <dim.horbach@gmail.com>
2015-03-11 18:03:01 +0300

    imagemagick into openocr

--- openocr/run.sh
+++ openocr/run.sh
@@ -1,4 +1,34 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "$0")" && pwd)"
-OUTPUTFILE="$2".json
-/opt/wftesseract/wf_tesseract $1 &>$OUTPUTFILE
+#!/bin/sh
+
+set -e
+
+: ${DPI:=300}
+
+with_dpi=0
+case $# in
+    2)
+	input="$1"
+	output="$2";;
+    3)
+	input="$1"
+	output="$2"
+	with_dpi=1
+	DPI="$3";;
+    *)
+	echo 'Invalid number of arguments' 1>&2
+	exit 2;;
+esac
+
+trap 'rm -f $tmp_file_tiff' 0
+tmp_file_tiff="$output.tmp.tiff"
+
+input_type=`file "$input" 2>/dev/null | awk '{print $2; exit}'`
+#echo "input_type=$input_type"
+
+case "${input_type}_${with_dpi}" in
+    TIFF_1|PNG_1|PDF_*)
+	convert -monochrome -density "$DPI" "$input" "$tmp_file_tiff"
+	input="$tmp_file_tiff";;
+esac
+
+${0%/*}/wf_tesseract "$input" > "$output"
