c464d007a1d3c9672b51ea6f8f385978962369cc
Dzmitry Horbach <dim.horbach@gmail.com>
2015-03-12 11:27:53 +0300

    wftesseract script fix

--- openocr/install.sh
+++ openocr/install.sh
@@ -2,6 +2,10 @@ SCRIPTPATH=`pwd -P`
 INSTALLDIR=/opt/wftesseract
 mkdir -p $INSTALLDIR
 cp -r $SCRIPTPATH/wf_tesseract $SCRIPTPATH/run.sh $INSTALLDIR
+
+ln -s $INSTALLDIR/run.sh /usr/local/bin/wftesseract
+ln -s $INSTALLDIR/wf_tesseract /usr/local/bin/wf_tesseract
+
 chmod +x $INSTALLDIR/run.sh
 chmod +x $INSTALLDIR/wf_tesseract
-ln -s $INSTALLDIR/run.sh /usr/local/bin/wftesseract
+
