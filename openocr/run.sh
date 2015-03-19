d0d2aea8a0c3b8a588363fdc885f4dbdd6b3b54d
Dzmitry Horbach <dim.horbach@gmail.com>
2015-03-19 11:06:07 +0300

    new version of tesseract script

--- openocr/run.sh
+++ openocr/run.sh
@@ -2,7 +2,31 @@
 
 set -e
 
-: ${DPI:=300}
+: ${DPI:=400}
+
+usage (){
+    cat <<'EOF'
+usage: wf_ocr [OPTIONS] input output [DPI]
+OPTIONS:
+  -g           display this help
+  -l lang      tesseract languages
+EOF
+}
+
+export WF_TESSERACT_LANG="eng+fax+dbank+jpm"; # the default
+
+while getopts hl: f; do
+    case "$f" in
+	'?')
+	    exit 1;;
+	h)
+	    usage
+	    exit 0;;
+	l)
+	    export WF_TESSERACT_LANG="$OPTARG";;
+    esac
+done
+shift `expr $OPTIND - 1`
 
 with_dpi=0
 case $# in
@@ -26,12 +50,15 @@ input_type=`file "$input" 2>/dev/null | awk '{print $2; exit}'`
 #echo "input_type=$input_type"
 
 case "${input_type}_${with_dpi}" in
+    cannot)
+	echo 'file(1) failed' 1>&2
+	exit 1;;
     TIFF_1|PNG_1|PDF_*)
-    convert -monochrome -density "$DPI" "$input" "$tmp_file_tiff"
-    input="$tmp_file_tiff";;
+	convert -monochrome -density "$DPI" "$input" "$tmp_file_tiff"
+	input="$tmp_file_tiff";;
     TIFF_0)
-    mv "$input" "$tmp_file_tiff"
-    input="$tmp_file_tiff";;
+	ln "$input" "$tmp_file_tiff"
+	input="$tmp_file_tiff";;
 esac
 
 ${0%/*}/wf_tesseract "$input" > "${output}.json"
